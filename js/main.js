a4fda70acad98bf2d8b0f539b570c694d415087a
Tameem Ahmad <tameemahmad49@gmail.com>
2020-10-28 19:54:52 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+
+//   collaborators 
+
+function myFunction() {
+    document.getElementById("colaBdoutput").style.visibility = "visible"; 
+    document.getElementById("colaBdoutput").style.opacity = "1"; 
+    document.getElementById("colaBdoutput").style.marginLeft = "40px"; 
+      document.getElementById("colaPhide").style.display = "none";
+  
+  }
+
+
+
+//   test
+
+
+
+
